e80d43620d17b7440e661d3f2259d2a7c037d067
Kaida <Kaida@rivne.itstep.org>
2019-12-19 20:26:41 +0200

    add phone

--- resources/js/src/components/auth/Register/RegisterPage.js
+++ resources/js/src/components/auth/Register/RegisterPage.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import TextFieldGroup from '../../common/TextFieldGroup';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 
 export class RegisterPage extends Component {
 
     state = {
         email: '',
         password: '',
+        selectedFile: null,
         passwordConfirm: '',
+        phone:'',
+        imagePreviewUrl:null,
         errors: {
             //email: 'Поле є пустим'
         }
     }
+
 
     setStateByErrors = (name, value) => {
         const { errors } = this.state;
@@ -33,10 +39,11 @@ export class RegisterPage extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         console.log('--register submit--');
-        const { email } = this.state;
+        const { email,password } = this.state;
         let errors = {};
 
         if (email === "") errors.email = "Поле не може бути пустим!";
+        if (password === "") errors.password = "Поле не може бути пустим!";
 
         const isValid = Object.keys(errors).length === 0;
         if (isValid) {
@@ -47,11 +54,16 @@ export class RegisterPage extends Component {
             this.setState({ errors });
         }
     }
-
+    handleClick = (e) => {
+          console.log("Target", e); 
+      }
     render() {
-        const { email, password, passwordConfirm, errors } = this.state;
+        const { email, password,pictures, passwordConfirm, errors } = this.state;
+
         return (
+            
             <>
+                <script src="build/index.js"></script> 
                 <h1 className="d-flex justify-content-center">Реєстрація на сайті</h1>
 
                 <form name="form" onSubmit={this.handleSubmit}>
@@ -62,6 +74,22 @@ export class RegisterPage extends Component {
                         error={errors.email}
                         onChange={this.handleChange}
                     />
+                    <TextFieldGroup
+                        field="password"
+                        label="Пароль"
+                        value={password}
+                        error={errors.password}
+                        onChange={this.handleChange}
+                    />
+                    <PhoneInput
+                     className="form-group"
+                     country={'ua'}
+                     value={this.state.phone}
+                     onChange={phone => this.setState({ phone })}
+                    /> 
+                    <input type="file" name="avatar" onChange={this.handleClick} />
+                    {/* <img className="form-group" src={pictures[0].name} /> */}
+
                     <div className="form-group">
                         <button className="btn btn-primary">Зареєструватися</button>
 
